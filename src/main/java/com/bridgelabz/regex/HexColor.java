48c2702f98d46996bb90cf3645ce0ebacf1e08b1
Nitesh2675 <162186102+Nitesh2675@users.noreply.github.com>
2025-04-01 23:52:39 +0530

    [Nitesh] ADD: hex color

--- src/main/java/com/bridgelabz/regex/HexColor.java
+++ src/main/java/com/bridgelabz/regex/HexColor.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.regex;
 
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class HexColor {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String input=sc.next();
+        String regex= "^#[0-9A-Fa-f]$";
+        Pattern pattern= Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(input);
+        if(matcher.matches()){
+            System.out.println("valid");
+        }
+        else{
+            System.out.println("Not valid");
+        }
+    }
 }
