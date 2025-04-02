226440a8cccd0a133edbc3574e997a8d6e7bd3c5
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-04-02 10:25:26 +0530

    [Nitesh] ADD: credit

--- src/main/java/com/bridgelabz/regex/Credit.java
+++ src/main/java/com/bridgelabz/regex/Credit.java
@@ -1,10 +1,23 @@
 package com.bridgelabz.regex;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Credit {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
        String input=sc.next();
+
+       String visaregex="4+[0-9]{15}";
+       String masterCardRegex="5+[0-9]{15}";
+
+       if(Pattern.matches(visaregex,input)){
+           System.out.println("valid visa");
+       }else if(Pattern.matches(masterCardRegex,input)) {
+           System.out.println("valid master");
+       }else{
+           System.out.println("not valid");
+       }
+
     }
 }
